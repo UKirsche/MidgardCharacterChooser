d95a01276e8e7cc491723fd0a2d2426695034f41
ukirsche <ukirschenmann@gmx.de>
2018-07-30 17:58:56 +0200

    Issue #1: Speicher Charakter in Toolbox

--- Scripts/HandleCharacterClick.cs
+++ Scripts/HandleCharacterClick.cs
@@ -8,12 +8,21 @@ using System.Linq;
 public class HandleCharacterClick : MonoBehaviour
 {
 	private FillCharacterSheet sheetManager;
+	private SheetFillFertigkeitenInventory sheetFertigkeiten;
+	private SheetFillWaffenInventory sheetWaffen;
+	private SheetFillZauberInventory sheetZauber;
 	public GameObject characterControler;
+	public GameObject fertigkeitenScroller;
+	public GameObject waffenScroller;
+	public GameObject zauberScroller;
 
 	// Use this for initialization
 	void Start ()
 	{
 		sheetManager = characterControler.GetComponent<FillCharacterSheet> ();
+		sheetFertigkeiten = fertigkeitenScroller.GetComponent<SheetFillFertigkeitenInventory> ();
+		sheetWaffen = waffenScroller.GetComponent<SheetFillWaffenInventory> ();
+		sheetZauber = zauberScroller.GetComponent<SheetFillZauberInventory> ();
 	}
 
 	void OnEnable(){
@@ -30,13 +39,20 @@ public class HandleCharacterClick : MonoBehaviour
 	}
 
 	/// <summary>
-	/// Handles the on item click. Lädt den Charakter in die UI
+	/// Handles the on item click. Lädt den Charakter in die UI und in die Toolbox
 	/// </summary>
 	/// <param name="itemDisplay">Item display.</param>
 	public void HandleOnItemClick (CharacterInventoryItemDisplay characterDisplay)
 	{
 		sheetManager.mCharacter = characterDisplay.item.mCharacter;
+		Toolbox globalVars = Toolbox.Instance;
+		globalVars.mCharacter = sheetManager.mCharacter;
 		sheetManager.SetCharacterValues ();
+		sheetFertigkeiten.FillPanel ();
+		sheetWaffen.FillPanel ();
+		sheetZauber.FillPanel ();
 
 	}
+
+
 }
