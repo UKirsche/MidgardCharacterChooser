80df45448bdc54713a663d2baf7d6ec63509b376
ukirsche <ukirschenmann@gmx.de>
2018-07-30 10:51:10 +0200

    Issue #1: Skripts neu FillCharacterSheet und HandleCharacterClick

---
+++ Scripts/HandleCharacterClick.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HandleCharacterClick : MonoBehaviour
+{
+
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	void OnEnable(){
+		InventoryItemBerufDisplay.onClick +=	HandleOnItemClick;
+	}
+
+	void OnDisable(){
+		InventoryItemBerufDisplay.onClick -= HandleOnItemClick;
+	}
+
+	void OnDestroy ()
+	{
+		InventoryItemBerufDisplay.onClick -= HandleOnItemClick;
+	}
+
+	/// <summary>
+	/// Handles the on item click. Controls whole process of adding and removing items.
+	/// </summary>
+	/// <param name="itemDisplay">Item display.</param>
+	public void HandleOnItemClick (InventoryItemDisplay itemDisplay)
+	{
+		
+	}
+}
