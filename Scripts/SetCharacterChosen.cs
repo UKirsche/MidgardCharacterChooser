4349f517063eb030a7c95c8e659866146361ce33
ukirsche <ukirschenmann@gmx.de>
2018-07-31 11:59:20 +0200

    Issue #5: chosenCharacter handling im button

---
+++ Scripts/SetCharacterChosen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCharacterChosen : MonoBehaviour {
+
+
+	public void SaveChosenCaracter(){
+		Toolbox globalVars = Toolbox.Instance;
+		MidgardCharakter mCharacterChosen = globalVars.mCharacter;
+
+		if (mCharacterChosen != null) {
+			MidgardCharacterSaveLoad.SaveChosen (mCharacterChosen);
+		}
+	}
+
+}
