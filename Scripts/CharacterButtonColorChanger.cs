588069c0ea04a86a8e705541e51d27f721fad5dd
ukirsche <ukirschenmann@gmx.de>
2018-08-01 11:25:15 +0200

    Issue #7 MouseOver funktionert neue files

---
+++ Scripts/CharacterButtonColorChanger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;  
+using System.Collections;  
+using UnityEngine.EventSystems;  
+using UnityEngine.UI;
+
+public class CharacterButtonColorChanger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	private Text theText;
+
+
+	void Start(){
+		theText = GetComponent<Text> ();
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		theText.color = Color.cyan; //Or however you do your color
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		theText.color = Color.white; //Or however you do your color
+	}
+
+
+}
