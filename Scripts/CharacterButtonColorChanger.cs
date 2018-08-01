7bd60e5cda68c0f2708dcda0661b29524d8b207d
ukirsche <ukirschenmann@gmx.de>
2018-08-01 12:25:22 +0200

    Issue #7: Click reset fertig

--- Scripts/CharacterButtonColorChanger.cs
+++ Scripts/CharacterButtonColorChanger.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class CharacterButtonColorChanger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
+	private const string CHARACTERITEMTAG = "characteritem";
 	private Text theText;
 
 
@@ -14,7 +15,7 @@ public class CharacterButtonColorChanger : MonoBehaviour, IPointerEnterHandler, 
 
 	public void OnPointerEnter(PointerEventData eventData)
 	{
-		theText.color = Color.cyan; //Or however you do your color
+		theText.color = new Color(0.41f, 0.72f, 1.63f); //Or however you do your color
 	}
 
 	public void OnPointerExit(PointerEventData eventData)
@@ -23,8 +24,22 @@ public class CharacterButtonColorChanger : MonoBehaviour, IPointerEnterHandler, 
 	}
 
 	public void ButtonClick(){
-		Image bgImage = transform.parent.gameObject.GetComponent<Image> ();
-		bgImage.color = Color.grey;
+		ResetButtonClickColors ();
+		ChangeClickedColor ();
 	}
 
+	private void ResetButtonClickColors(){
+		GameObject[] buttons = GameObject.FindGameObjectsWithTag (CHARACTERITEMTAG);
+
+		foreach (var button in buttons) {
+			Image buttonImage = button.GetComponent<Image> ();
+			buttonImage.color = new Color (0.1f, 0.1f, 0.13f, 0.45f);
+		}
+	}
+
+	void ChangeClickedColor ()
+	{
+		Image bgImage = transform.parent.gameObject.GetComponent<Image> ();
+		bgImage.color = new Color (0.1f, 0.1f, 0.13f, 0.64f);
+	}
 }
